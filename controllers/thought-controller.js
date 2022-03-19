b35cd429a1a59cab4925b34e3400306c7280a77d
Zachary Clark <zacclark369@gmail.com>
2022-03-19 13:46:38 -0700

    started outlining controller files, and user model. Finished index.js for models, very simple

---
+++ controllers/thought-controller.js
@@ -0,0 +1,9 @@
+const { Thought, User } = require('../models');
+
+const thoughtController = {
+    addThought
+
+    
+}
+
+module.exports = thoughtController;
---
+++ controllers/user-controller.js
@@ -0,0 +1,15 @@
+const { User } = require('../models');
+
+const userController = {
+    getAllUsers
+
+    getUserbyId
+
+    createUser
+
+    updateUser
+
+    deleteUser
+}
+
+module.exports = userController;
